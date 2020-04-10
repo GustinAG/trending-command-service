b9b47fc6b3e71f614b9ab72bc430f6f172d98ef3
Gustin AG <gustinn@gmail.com>
2020-04-10 22:18:50 +0200

    version info on API root

---
+++ Trending.Command.Api/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trending.Command.Api.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            var assembly = GetType().Assembly;
+            var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+            var name = versionInfo.ProductName;
+            var version = assembly.GetName().Version.ToString();
+            var copyright = versionInfo.LegalCopyright;
+
+            return $"{name} v{version} - {copyright}";
+        }
+    }
+}
